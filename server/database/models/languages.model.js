f11f7e9e1de2f9274e20cabbf478db1c91409f70
lilia-g <liliagh2024@gmail.com>
2025-02-04 15:53:29 +0100

    offfff

---
+++ server/database/models/languages.model.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+    const Language = sequelize.define('Language', {
+      language_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      }
+    }, {
+      timestamps: false // Disable createdAt and updatedAt fields
+    });
+  
+    return Language;
+  };
+  
---
+++ server/database/models/userlanguages.model.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+    const UserLanguages = sequelize.define('UserLanguages', {
+      learning_progress: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+      }
+    }, {
+      timestamps: true,
+    
+    });
+  
+    return UserLanguages;
+  };
